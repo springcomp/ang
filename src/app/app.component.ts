84c34b906b83b38e6ac38ba634e83ab8761d9cc7
Maxime LABELLE <maxime.labelle@chanel.com>
2022-12-20 08:33:13 +0100

    Only focus on editing Markdown - rendering will be done separately.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import {
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { MarkdownModule, MarkdownService } from 'ngx-markdown';
 import { CustomInputComponent } from './custom-input/custom-input.component';
-import { MyTel, MyTelInput } from './tel-input/tel-input.component';
+import { MarkdownEditorComponent } from './markdown-editor/markdown-editor.component';
 
 @Component({
   standalone: true,
@@ -23,7 +23,7 @@ import { MyTel, MyTelInput } from './tel-input/tel-input.component';
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MyTelInput,
+    MarkdownEditorComponent,
     MatInputModule,
     MarkdownModule,
   ]
@@ -39,7 +39,7 @@ export class AppComponent {
     library.addIconPacks(fas);
     this.form = this.formBuilder.group<FormData>({
       myFormControl: new FormControl('initial input three', { validators: Validators.required, nonNullable: true }),
-      phone: new FormControl(new MyTel('# Overview\nThis is a simple **bold** text.')),
+      markdown: new FormControl('# Overview\nThis is a simple **bold** text.'),
       one: new FormControl(''),
       two: new FormControl('')
     });
@@ -47,13 +47,13 @@ export class AppComponent {
   }
 
   public getValue(event: UIEvent): void {
-    const phone = this.form.value.phone?.toString();
+    const phone = this.form.value.markdown?.toString();
     console.log(phone);
   }
 
   public renderMarkdown(): string {
     // console.log(this.markdownService.options);
-    const text = this.form.get('phone')?.value?.toString() ?? '';
+    const text = this.form.value.markdown?.toString() ?? '';
     const html = this.markdownService.parse(text);
     return html;
   }
@@ -61,7 +61,7 @@ export class AppComponent {
 
 export class FormData {
   myFormControl!: FormControl<string | null>;
-  phone!: FormControl<MyTel | null>;
+  markdown!: FormControl<string | null>;
   one!: FormControl<string | null>;
   two!: FormControl<string | null>;
 }
--- src/app/markdown-editor/markdown-editor.component.ts
+++ src/app/markdown-editor/markdown-editor.component.ts
@@ -5,7 +5,7 @@ import {
   forwardRef,
   HostBinding,
   Input,
-  OnInit,
+  OnDestroy,
 } from '@angular/core';
 import {
   ControlValueAccessor,
@@ -16,38 +16,26 @@ import {
   NG_VALUE_ACCESSOR,
   ReactiveFormsModule,
 } from '@angular/forms';
-import { FocusMonitor } from '@angular/cdk/a11y';
 import { MatFormFieldControl } from '@angular/material/form-field';
-import { Subject } from 'rxjs';
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import '@github/markdown-toolbar-element';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import * as EasyMDE from 'easymde';
-
-export class MyTel {
-  constructor(public area: string,
-    public preview: string = '') { }
-
-  toString(): string {
-    return this.area;
-  }
-}
-
-// https://dev.to/trungk18/build-a-markdown-editor-with-angular-14m0
+import '@github/markdown-toolbar-element';
+import { Subject } from 'rxjs';
 
 @Component({
   standalone: true,
-  selector: 'my-tel-input',
-  templateUrl: './tel-input.component.html',
-  styleUrls: ['./tel-input.component.scss'],
+  selector: 'markdown-editor',
+  templateUrl: './markdown-editor.component.html',
+  styleUrls: ['./markdown-editor.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => MyTelInput),
+      useExisting: forwardRef(() => MarkdownEditorComponent),
       multi: true,
     },
-    { provide: MatFormFieldControl, useExisting: MyTelInput },
+    { provide: MatFormFieldControl, useExisting: MarkdownEditorComponent },
   ],
   host: {
     '[id]': 'id',
@@ -55,57 +43,61 @@ export class MyTel {
   },
   imports: [FormsModule, ReactiveFormsModule, FontAwesomeModule],
 })
-export class MyTelInput
-  implements OnInit, ControlValueAccessor, MatFormFieldControl<MyTel>
+export class MarkdownEditorComponent
+  implements OnDestroy, ControlValueAccessor, MatFormFieldControl<string>
 {
-  parts: FormGroup;
-
-  @Input()
-  get value(): MyTel | null {
-    let n = this.parts.value;
-    return new MyTel(n.area);
-  }
-  set value(tel: MyTel | null) {
-    tel = tel || new MyTel('');
-    this.parts.setValue({
-      area: tel.area,
-      preview: '',
-    });
-    this.onPropagateChanges();
-  }
+  form: FormGroup;
 
   constructor(
-    fb: FormBuilder,
-    private fm: FocusMonitor,
+    formBuilder: FormBuilder,
+    private focusMonitor: FocusMonitor,
     private element: ElementRef
   ) {
-    this.parts = fb.group({
-      area: '',
-      preview: '',
-    });
-    fm.monitor(element.nativeElement, true).subscribe((origin) => {
+    this.form = formBuilder.group({ markdown: '' });
+    focusMonitor.monitor(element.nativeElement, true).subscribe((origin) => {
       this.focused = !!origin;
       this.onPropagateChanges();
     });
   }
-  hidden: boolean = false;
-  onChange: any = () => { };
-  onTouch: any = () => { };
+
+  ngOnDestroy() {
+    this.stateChanges.complete();
+    this.focusMonitor.stopMonitoring(this.element.nativeElement);
+  }
+
+  @Input()
+  get value(): string | null {
+    return this.form.value.markdown;
+  }
+  set value(text: string | null) {
+    text = text || '';
+    this.form.setValue({ markdown: text });
+    this.onPropagateChanges();
+  }
+
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
   registerOnTouched(fn: any): void {
     this.onTouch = fn;
   }
-  writeValue(obj: MyTel): void {
-    this.value = obj;
+
+  writeValue(text: string): void {
+    this.value = text;
   }
 
   stateChanges = new Subject<void>();
 
+  static selector = 'markdown-editor';
   static nextId = 0;
 
-  @HostBinding() id = `my-tel-input-${MyTelInput.nextId++}`;
+  @HostBinding() id = `${
+    MarkdownEditorComponent.selector
+  }-${MarkdownEditorComponent.nextId++}`;
+
   @Input()
   get placeholder() {
     return this._placeholder;
@@ -115,16 +107,21 @@ export class MyTelInput
     this.onPropagateChanges();
   }
   private _placeholder: string = '';
+
   ngControl: NgControl | null = null;
+
   focused = false;
+
   get empty() {
-    let n = this.parts.value;
+    let n = this.form.value;
     return !n.area && !n.exchange && !n.subscriber;
   }
+
   @HostBinding('class.floating')
   get shouldLabelFloat() {
     return this.focused || !this.empty;
   }
+
   @Input()
   get required() {
     return this._required;
@@ -134,6 +131,7 @@ export class MyTelInput
     this.onPropagateChanges();
   }
   private _required = false;
+
   @Input()
   get disabled() {
     return this._disabled;
@@ -142,37 +140,28 @@ export class MyTelInput
     this._disabled = coerceBooleanProperty(dis);
   }
   private _disabled = false;
-  errorState = false;
-  controlType = 'my-tel-input';
-  @HostBinding('attr.aria-describedby') describedBy = '';
 
+  errorState = false;
+
+  controlType = MarkdownEditorComponent.selector;
+
+  @HostBinding('attr.aria-describedby') describedBy = '';
   setDescribedByIds(ids: string[]) {
     this.describedBy = ids.join(' ');
   }
+
   onContainerClick(event: MouseEvent): void {
     if ((event.target as Element).tagName.toLowerCase() != 'textarea') {
       this.element.nativeElement.querySelector('textarea').focus();
     }
   }
-  togglePreview(): void {
-    this.hidden = !this.hidden;
-  }
+
   propagateChanges(event: Event): void {
     this.onPropagateChanges();
   }
+
   onPropagateChanges(): void {
     this.onChange(this.value);
     this.stateChanges.next();
-  }
-
-  ngOnInit(): void {
-    // const easyMDE = new EasyMDE({
-    //  element: document.getElementById('textarea_id') ?? undefined,
-    // });
-  }
-
-  ngOnDestroy() {
-    this.stateChanges.complete();
-    this.fm.stopMonitoring(this.element.nativeElement);
   }
 }
